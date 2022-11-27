74a3265140e9bd90b9c6fb27588d59096cc9ae41
Paul B <69534608+paulbgomez@users.noreply.github.com>
2022-11-27 19:43:40 +0100

    Clase usuario

--- UTF3_CRIPTOGRAFIA/src/Menu/Menu.java
+++ UTF3_CRIPTOGRAFIA/src/Menu/Menu.java
@@ -1,140 +1,150 @@
 package Menu;
 
-import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import Usuario.Usuario;
+
 
 
 public class Menu {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		
-		try {
-		// Primero creamos todos los objetos necesarios para el cifrado simétrico:
-		// Creamos el generador de claves, elegimos el algoritmo AES	
-			
-		KeyGenerator generadorSimetrica = KeyGenerator.getInstance("AES");
+		int intentos = 0;
 		
-		
-		// Creamos la clave simétrica con la que vamos a encriptar:
-		
-		SecretKey claveSecreta = generadorSimetrica.generateKey();
-		
-		// Creamos el objeto que va a cifrar y descifrar las frases y objetos con la clave, 
-		// le pasamos el algoritmo que usa para cifrar, que será el AES
-		
-		Cipher cifradorSimetrico = Cipher.getInstance("AES");
-		
-		
-		// Lo iniciamos o configuramos para encriptar:
-		cifradorSimetrico.init(Cipher.ENCRYPT_MODE, claveSecreta);
-		
-		
-		// Creamos un objeto Cipher para desencriptado simétrico:
-		
-		Cipher descifradorSimetrico = Cipher.getInstance("AES");
-		
-		// Lo iniciamos o configuramos para desencriptar:
-		
-		descifradorSimetrico.init(Cipher.DECRYPT_MODE, claveSecreta);
-		
-		// Creamos la variable que almacena la frase original
-		
-		String frase = null;
-		
-		// Creamos la variable que almacena el flujo de bytes de la frase original introducida:
-		
-		byte [] bytesFrase = null;
-		
-		// Creamos la variable que almacenerá en memoria el flujo de bytes con la frase cifrada 
-					
-		byte [] bytesFraseCifrada = null;
-		
-		// Creamos la variable tipo String donde se almacenará la frase encriptada
-					
-		String fraseCifrada = null;
-		
-		// Variable que almacena los bytes de la frase tras descifrarla
-		
-		byte [] bytesFraseDescifrada = null;
-		
-		// Creamos una variable boolean para controlar la repetición del bucle.
-					
-		boolean continuar = true;
-		
-		do {
-			// Creamos el manú para que salga por pantalla y permita escoger una opción 
-			System.out.println("ELIJA UNA OPCIÓN:\n\n"
-					+ "  0. Salir \n"
-					+ "  1. Encriptar frase \n"
-					+ "  2. Mostrar frase encriptada \n"
-					+ "  3. Desencriptar frase \n"
-					);
-			
+		String password1 = hashPassword("password");
+		String password2 = hashPassword("123456");
+		String password3 = hashPassword("gatocan");
 
-			// Creamos un objeto Scanner para pedir por pantalla la opcion del menú deseada
-			Scanner sc = new Scanner(System.in);
-			int opcion = sc.nextInt();	
-			// Creamos un único Scanner para usar a lo largo del menú que lea Strings
-			Scanner scanner = new Scanner(System.in);
-		switch (opcion) {
+		Usuario user1 = new Usuario("paul", password1);
+		Usuario user2 = new Usuario("gonzalo", password2);
+		Usuario user3 = new Usuario("sandra", password3);
 		
-		case 0:
-			System.out.println("----SALIR----");
-			System.out.println("La aplicación ha terminado ");
-			continuar = false;
-			break;
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(user1);
+		usuarios.add(user2);
+		usuarios.add(user3);
+				
+		Usuario logged = logIn();
 		
-		case 1:
-			System.out.println("----ENCRIPTAR FRASE----");
-			// Pedimos la frase por consola y la almacenamos en una variable de tipo String:
-			System.out.println("Escriba la frase que desea encriptar:");
-			frase = scanner.nextLine();
-			// Convertimos el String recibido en un array o flujo de bytes para poder cifrar la frase
-			bytesFrase = frase.getBytes();
-			
-			bytesFraseCifrada = cifradorSimetrico.doFinal(bytesFrase);
-			// Guardamos en la variable tipo String fraseCifrada el valor del flujo de bytes de la frase cifrada pasado a String
-			fraseCifrada = new String(bytesFraseCifrada);
-			System.out.println("La frase ha sido cifrada con cifrado simétrico");
-			
-
-		break;
-			
-		case 2:
-			System.out.println("----MOSTRAR FRASE ENCRIPTADA----");
-			// Mostramos la frase original:
-			System.out.println("La frase original era: " + frase);
-			// Mostramos por pantalla la frase encriptada almacenada en memoria
-			System.out.println("La frase encriptada es la siguiente: " + fraseCifrada);
-			break;
-		
-		case 3:
-			System.out.println("----DESCIFRAR FRASE----");
-			
-			
-			// Guardamos en un flujo de bytes la frase desencriptada:
-			bytesFraseDescifrada = descifradorSimetrico.doFinal(bytesFraseCifrada);
-			// Mostramos la frase descifrada por pantalla, pasándola a String
-			System.out.println("La frase desencriptada es: " + new String(bytesFraseDescifrada));
-			
-			
-			break;
-
-			
+		// falta hacer que si los intentos son > 3 se cierre la aplicacion
+		for (Usuario usuario : usuarios) {
+			if (logged.equals(usuario)) {
+				System.out.println("login correcto");
+			}
 		}
 		
-		}	while (continuar); 
+		try {
+			// Primero creamos todos los objetos necesarios para el cifrado simï¿½trico:
+			// Creamos el generador de claves, elegimos el algoritmo AES		
+			KeyGenerator generadorSimetrica = KeyGenerator.getInstance("AES");
+			
+			// Creamos la clave simï¿½trica con la que vamos a encriptar:
+			SecretKey claveSecreta = generadorSimetrica.generateKey();
+			
+			// Creamos el objeto que va a cifrar y descifrar las frases y objetos con la clave, 
+			// le pasamos el algoritmo que usa para cifrar, que serï¿½ el AES
+			Cipher cifradorSimetrico = Cipher.getInstance("AES");
+			
+			
+			// Lo iniciamos o configuramos para encriptar:
+			cifradorSimetrico.init(Cipher.ENCRYPT_MODE, claveSecreta);
+			
+			// Creamos un objeto Cipher para desencriptado simï¿½trico:
+			Cipher descifradorSimetrico = Cipher.getInstance("AES");
+			
+			// Lo iniciamos o configuramos para desencriptar:
+			descifradorSimetrico.init(Cipher.DECRYPT_MODE, claveSecreta);
+			
+			// Creamos la variable que almacena la frase original y la cifrada
+			String frase = null;
+			String fraseCifrada = null;
+			
+			// Creamos una variable boolean para controlar la repeticiï¿½n del bucle.			
+			boolean continuar = true;
+			
+			do {
+				// Creamos el manï¿½ para que salga por pantalla y permita escoger una opciï¿½n 
+				System.out.println("ELIJA UNA OPCIï¿½N:\n\n"
+						+ "  0. Salir \n"
+						+ "  1. Encriptar frase \n"
+						+ "  2. Mostrar frase encriptada \n"
+						+ "  3. Desencriptar frase \n"
+						);
+
+				// Creamos un objeto Scanner para pedir por pantalla la opcion del menï¿½ deseada
+				Scanner sc = new Scanner(System.in);
+				int opcion = sc.nextInt();	
+
+				switch (opcion) {
+					case 0:
+						System.out.println("----SALIR----");
+						System.out.println("La aplicaciï¿½n ha terminado ");
+						continuar = false;
+						break;
+					case 1:
+						System.out.println("----ENCRIPTAR FRASE----");
+						// Pedimos la frase por consola y la almacenamos en una variable de tipo String:
+						System.out.println("Escriba la frase que desea encriptar:");
+						Scanner scanner = new Scanner(System.in);
+						frase = scanner.nextLine();
+						// Convertimos el String recibido en un array o flujo de bytes para poder cifrar la frase
+						byte[] bytesFrase = frase.getBytes();
+						byte[] bytesFraseCifrada = cifradorSimetrico.doFinal(bytesFrase);
+						// Guardamos en la variable tipo String fraseCifrada el valor del flujo de bytes de la frase cifrada pasado a String
+						fraseCifrada = new String(bytesFraseCifrada);
+						System.out.println("La frase ha sido cifrada con cifrado simï¿½trico");
+						break;
+					case 2:
+						System.out.println("----MOSTRAR FRASE ENCRIPTADA----");
+						// Mostramos la frase original:
+						System.out.println("La frase original era: " + frase);
+						// Mostramos por pantalla la frase encriptada almacenada en memoria
+						System.out.println("La frase encriptada es la siguiente: " + fraseCifrada);
+						break;
+					case 3:
+						System.out.println("----DESCIFRAR FRASE----");
+						// Guardamos en un flujo de bytes la frase desencriptada:
+						byte[] bytesFraseDescifrada = descifradorSimetrico.doFinal(fraseCifrada.getBytes());
+						// Mostramos la frase descifrada por pantalla, pasï¿½ndola a String
+						System.out.println("La frase desencriptada es: " + new String(bytesFraseDescifrada));
+						break;
+				}
+			}	while (continuar); 
+		} catch (Exception e) {
+			System.out.println("Un error ha ocurrido... " + e.getMessage());
+			e.printStackTrace();
+		}
 		
-	} catch (Exception e) {
-		System.out.println("Un error ha ocurrido... " + e.getMessage());
-		e.printStackTrace();
-	}
 	}
 
-
+	public static String hashPassword(String password) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		md.update(password.getBytes());
+		String mensajeHashBase64 = Base64.getEncoder().encodeToString(md.digest());
+		return mensajeHashBase64;
+	}
+	
+	public static Usuario logIn() throws NoSuchAlgorithmException {
+		System.out.println("Introduzca su usuario: \n");
+		Scanner scanner = new Scanner(System.in);
+		String nombre = scanner.nextLine();
+		
+		System.out.println("Introduzca su password: \n");
+		String password = hashPassword(scanner.nextLine());
+		
+		
+		Usuario usuario = new Usuario(nombre, password);
+		return usuario;
+	}
+	
 }
